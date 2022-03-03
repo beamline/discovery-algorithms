9277489f6c3a54d2d2b9fdcef7830ed6b2b964ae
Andrea Burattin <andrea.burattin@gmail.com>
2022-03-03 10:39:20 +0100

    Isolated code for trivial miner

--- src/main/java/beamline/miners/dcr/model/patterns/RelationPattern.java
+++
@@ -1,8 +0,0 @@
-package beamline.miners.dcr.model.patterns;
-
-import beamline.miners.dcr.model.relations.UnionRelationSet;
-
-public interface RelationPattern {
-    public void populateConstraint(UnionRelationSet unionRelationSet);
-
-}
